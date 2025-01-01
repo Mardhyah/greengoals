15ec59a4ec1a5235b76d1add7232901da92eb7f0
Mardhyah <mardhyahfathaniaizzati9d@gmail.com>
2025-01-01 17:49:02 +0700

    add pesanan

--- lib/screans/pesanan_screen/pesanan_screen.dart
+++ lib/screans/pesanan_screen/pesanan_screen.dart
@@ -1,16 +1,13 @@
 import 'package:flutter/material.dart';
 
-import 'package:greengoals/themes/color_scheme.dart';
-import 'package:greengoals/themes/text_theme.dart';
+import '../../themes/color_scheme.dart';
+import '../../themes/text_theme.dart';
+import 'components/diproses.dart';
+import 'components/menunggu_pesanan.dart';
 
-class PesananScreen extends StatefulWidget {
-  const PesananScreen({super.key});
+class PesananScreen extends StatelessWidget {
+  const PesananScreen({Key? key}) : super(key: key);
 
-  @override
-  State<PesananScreen> createState() => _PesananScreenState();
-}
-
-class _PesananScreenState extends State<PesananScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,6 +18,42 @@ class _PesananScreenState extends State<PesananScreen> {
           style: semiBoldBody1.copyWith(color: whiteColor),
         ),
         centerTitle: true,
+      ),
+      body: DefaultTabController(
+        length: 5,
+        child: Column(
+          children: [
+            Container(
+              constraints: const BoxConstraints.expand(height: 50),
+              child: const SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: TabBar(
+                  isScrollable: true,
+                  tabs: [
+                    Tab(
+                        child: Text('Menunggu Konfirmasi',
+                            style: TextStyle(fontSize: 13))),
+                    Tab(child: Text('Proses', style: TextStyle(fontSize: 13))),
+                    Tab(child: Text('Dikirim', style: TextStyle(fontSize: 13))),
+                    Tab(child: Text('Selesai', style: TextStyle(fontSize: 13))),
+                    Tab(child: Text('Gagal', style: TextStyle(fontSize: 13))),
+                  ],
+                  indicatorColor: Colors.black,
+                  labelColor: Colors.black,
+                  unselectedLabelColor: Colors.grey,
+                ),
+              ),
+            ),
+            const Expanded(
+              child: TabBarView(
+                children: [
+                  WaitingConfirmationTab(title: 'Menunggu Konfirmasi'),
+                  Diproses(title: 'Menunggu Konfirmasi'),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
